ceeab169926e2b63796c15a60c8d079e016c81ad
rgu <gubeini@seas.upenn.edu>
2022-12-01 11:53:52 -0500

    Move cube to player, change gravity, reset ball

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -21,11 +21,28 @@ public class BallScript : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Collider collider = collision.collider;
-        if (collider.CompareTag("10PtTileTag")) {
+        if (collider.CompareTag("PlayerTag")) {
+            // Set speed to 0
+            Debug.Log("Collide with player");
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.angularVelocity = new Vector3(0, 0, 0);
+        } else if (collider.CompareTag("BackWallTag"))
+        {
+            // Reset ball
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.angularVelocity = new Vector3(0, 0, 0);
+            this.transform.position = GameObject.Find("Player").transform.position + new Vector3(0, 0, .1f); // Does not spawn at correct position
+        }
+        else if (collider.CompareTag("10PtTileTag"))
+        {
             global.score += 10;
-        } else if (collider.CompareTag("20PtTileTag")) {
+        } else if (collider.CompareTag("20PtTileTag"))
+        {
             global.score += 20;
-        } else if (collider.CompareTag("30PtTileTag")) {
+        } else if (collider.CompareTag("30PtTileTag"))
+        {
             global.score += 30;
         }
     }
