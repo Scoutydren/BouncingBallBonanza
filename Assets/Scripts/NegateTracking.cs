942fae9697eaa738667010545bd8b8c545c72c92
rgu <gubeini@seas.upenn.edu>
2022-12-05 12:57:55 -0500

    Fix player in place

--- Assets/Scripts/NegateTracking.cs
+++ Assets/Scripts/NegateTracking.cs
@@ -5,16 +5,19 @@ using UnityEngine.XR;
 
 public class NegateTracking : MonoBehaviour
 {
+    Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.startPosition = new Vector3(-0.07f, 0.5f, -2.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = -InputTracking.GetLocalPosition(XRNode.CenterEye);
+        transform.position = -InputTracking.GetLocalPosition(XRNode.CenterEye) + startPosition;
+        //transform.position = new Vector3(0, 0, 0);
         //transform.rotation = Quaternion.Inverse(InputTracking.GetLocalRotation(XRNode.CenterEye));
     }
 }
