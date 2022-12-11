02b466b0c95cc458dfe33f8d7f344e2f1a4d6236
tienp10 <tienp@seas.upenn.edu>
2022-12-11 09:53:36 -0500

    Menu reset ball when too far

--- Assets/Scripts/MenuBallScript.cs
+++ Assets/Scripts/MenuBallScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuBallScript : MonoBehaviour
 {
+    private int threshold;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.threshold = 30;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Math.Abs(this.transform.position[0]) > threshold || Math.Abs(this.transform.position[1]) > threshold || Math.Abs(this.transform.position[2]) > threshold)
+        {
+            // Reset position
+            this.transform.position = new Vector3(0, 1f, 0.5f);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
